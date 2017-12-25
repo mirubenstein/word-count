51e91c315f29ab269a6697ff90804a385bf1d469
Mark R <mirubenstein@gmail.com>
2017-12-25 09:50:42 -0800

    Update regex and iterator variable name.

--- word_count.rb
+++ word_count.rb
@@ -6,8 +6,8 @@ class Phrase
   end
 
   def word_count
-    phrase.downcase.scan(/\w+'\w+|\w+/).each_with_object(Hash.new(0)) do |word, word_to_count|
-      word_to_count[word] += 1
+    phrase.downcase.scan(/\b[\w']+\b/).each_with_object(Hash.new(0)) do |word, word_count_hash|
+      word_count_hash[word] += 1
     end
   end
 end
